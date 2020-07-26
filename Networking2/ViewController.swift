ddf38131bc6b9ca63a9e3b33b59d629190710b39
Lisa <tramzaika@gmail.com>
2020-07-26 10:27:27 +0700

    Initial Commit

---
+++ Networking2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Networking2
+//
+//  Created by 1234 on 26.07.2020.
+//  Copyright © 2020 Lisa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
